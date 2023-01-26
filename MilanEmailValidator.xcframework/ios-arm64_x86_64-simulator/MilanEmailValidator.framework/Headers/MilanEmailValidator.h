c40726081351e59a97499020f82ce879be9da6b7
mcvetkovic-symphony <milan.cvetkovic@symphony.is>
2023-01-26 21:17:53 +0100

    add xcframework

---
+++ MilanEmailValidator.xcframework/ios-arm64_x86_64-simulator/MilanEmailValidator.framework/Headers/MilanEmailValidator.h
@@ -0,0 +1,18 @@
+//
+//  MilanEmailValidator.h
+//  MilanEmailValidator
+//
+//  Created by Milan Cvetkovic on 24.1.23..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MilanEmailValidator.
+FOUNDATION_EXPORT double MilanEmailValidatorVersionNumber;
+
+//! Project version string for MilanEmailValidator.
+FOUNDATION_EXPORT const unsigned char MilanEmailValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MilanEmailValidator/PublicHeader.h>
+
+
